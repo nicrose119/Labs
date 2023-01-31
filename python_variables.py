a9d1655c4ee8d865f47c69f6a9e55b2db1bec089
Nicole Overholt <nicrose119@gmail.com>
2023-01-30 22:28:34 -0500

    Initialized repository for all labs. Completed Day Labs python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,23 @@
+#Task1
+age = 41
+print(f"I am {age} years old")
+
+#Task2
+first_name = input("What is your first name? ")
+print(first_name)
+
+#Task3
+last_name = input("What is your last name? ")
+print(last_name)
+
+#Task4
+print(f'{first_name} {last_name}')
+
+full_name = f'{first_name} {last_name}'
+print(f'My first name is {first_name} and my last name is {last_name}, which means my full name is {full_name}')
+
+#Task5
+fahrenheit_temp = 18
+celcius_temp = ((fahrenheit_temp-32) / 1.8)
+print(celcius_temp)
+print(f'{fahrenheit_temp} degrees Fahrenheit is {celcius_temp} degrees Celcius')
